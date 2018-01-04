a2904e2ca7d3a35285310f5686682504ac2561c9
Christopher Dorst <chris@dorst.email>
2018-01-03 23:47:30 -0600

    Adding CDorst account defaults

--- DevOps.VersionControl.Functions.GetGitHubRepoRemoteUri/RemoteUriGetter.cs
+++ DevOps.VersionControl.Functions.GetGitHubRepoRemoteUri/RemoteUriGetter.cs
@@ -1,20 +1,22 @@
 ﻿using Common.Structures.HttpBasicAuthentication;
 using DevOps.VersionControl.Structures.GitHub;
 using System;
+using static Metaproject.Users.CDorst.GitHubAccessToken.GitHubCredentials;
 
 namespace DevOps.VersionControl.Functions.GetGitHubRepoRemoteUri
 {
     public static class RemoteUriGetter
     {
+        private const string cdorst = nameof(cdorst);
         private const string Root = "https://github.com/";
 
         public static Uri RemoteUri(AccountRepository accountRepository)
             => new Uri($"{accountRepository.Uri}.git");
 
-        public static Uri RemoteUri(string account, string repository)
+        public static Uri RemoteUri(string repository, string account = cdorst)
             => RemoteUri(new AccountRepository(account, repository));
 
-        public static Uri RemoteUri(BasicAuthenticationCredentials credentials, string repository)
-            => RemoteUri(credentials.User, repository);
+        public static Uri RemoteUri(string repository, BasicAuthenticationCredentials? credentials)
+            => RemoteUri((credentials ?? CDorst).User, repository);
     }
 }

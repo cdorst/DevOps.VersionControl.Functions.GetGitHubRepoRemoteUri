1384d5f461fb5ba54cea79fb2541be337329f316
Christopher Dorst <chris@dorst.email>
2018-01-03 23:33:31 -0600

    Add RemoteUriGetter

---
+++ DevOps.VersionControl.Functions.GetGitHubRepoRemoteUri/RemoteUriGetter.cs
@@ -0,0 +1,20 @@
+﻿using Common.Structures.HttpBasicAuthentication;
+using DevOps.VersionControl.Structures.GitHub;
+using System;
+
+namespace DevOps.VersionControl.Functions.GetGitHubRepoRemoteUri
+{
+    public static class RemoteUriGetter
+    {
+        private const string Root = "https://github.com/";
+
+        public static Uri RemoteUri(AccountRepository accountRepository)
+            => new Uri($"{accountRepository.Uri}.git");
+
+        public static Uri RemoteUri(string account, string repository)
+            => RemoteUri(new AccountRepository(account, repository));
+
+        public static Uri RemoteUri(BasicAuthenticationCredentials credentials, string repository)
+            => RemoteUri(credentials.User, repository);
+    }
+}
